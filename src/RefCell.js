16abf0e5aaa190badb4677cf9982d53148749760
Brett Deaton <brett.deaton@gmail.com>
2023-05-13 19:18:11 -0700

    Use new braille translation method

--- src/RefCell.js
+++ src/RefCell.js
@@ -4,7 +4,7 @@ import "./RefCell.css";
 function RefCell({ char }) {
   return (
     <div className="ref-cell">
-      <p className="braille">{br.toBraille(char)}</p>
+      <p className="braille">{br.singleToBraille(char)}</p>
       <p className="english">{char}</p>
     </div>
   );
