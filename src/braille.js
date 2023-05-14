bbec35f326376c96c002032bdcbda977859bd7e3
Brett Deaton <brett.deaton@gmail.com>
2023-05-13 19:18:45 -0700

    Introduce multi-token translation

--- src/braille.js
+++ src/braille.js
@@ -8,11 +8,40 @@ export default class Braille {
     return Object.fromEntries(Object.entries(obj).map(([k, v]) => [v, k]));
   }
 
-  static toAscii(braille) {
+  static singleToAscii(braille) {
+    if (braille.length !== 1) {
+      throw new Error("Arg must be a single unicode code point");
+    }
+    if (
+      braille.charCodeAt(0) < parseInt("2800", 16) ||
+      braille.charCodeAt(0) > parseInt("283F", 16)
+    ) {
+      throw new Error("Arg must have codepoint in range [0x2800, 0x283F]");
+    }
     return Braille.mapBToA[braille];
   }
 
-  static toBraille(ascii) {
+  static toBraille(string) {
+    const tokens = string.split("");
+    let trans = [];
+    for (let i = 0; i < tokens.length; ) {
+      let start = i;
+      let next = i + 1;
+      if (tokens[i] === "(") {
+        next = start + tokens.slice(start).indexOf(")") + 1;
+      } else if (tokens[i] === "[") {
+        next = start + tokens.slice(start).indexOf("]") + 1;
+      }
+      trans.push(Braille.singleToBraille(tokens.slice(start, next).join("")));
+      i = next;
+    }
+    return trans.join("");
+  }
+
+  static singleToBraille(ascii) {
+    if (!(ascii in Braille.mapAToB)) {
+      throw new Error(`Translation of ${ascii} not found`);
+    }
     return Braille.mapAToB[ascii.toLowerCase()];
   }
 }
