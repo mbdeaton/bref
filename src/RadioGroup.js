5205523679b2f93433266201e863404939f532c7
Brett Deaton <brett.deaton@gmail.com>
2023-06-10 21:53:34 -0700

    Remove warning "checked without onChange"

--- src/RadioGroup.js
+++ src/RadioGroup.js
@@ -1,12 +1,13 @@
 function RadioGroup({ names, handleChange, displayOrder, ...props }) {
   return (
-    <form onChange={handleChange} {...props}>
+    <form {...props}>
       {names.map((name) => (
         <label key={name}>
           <input
             type="radio"
             name="display"
             value={name}
+            onChange={handleChange}
             checked={name === displayOrder}
           />
           <span>{name}</span>
